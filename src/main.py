37ae5457ac638c75b43174424fb2537df4f82acc
SCRIPTERBLOX <scriptoblox@gmail.com>
2025-11-06 13:22:22 +0000

    Inserted simple tkinter example

--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
+from tkinter import *
+from tkinter import ttk
 
+
+root = Tk()
+root.title("LinuxMaker")
+
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+ttk.Label(mainframe, text="Hello there").grid(column=3, row=1, sticky=N)
+
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Button", command=calculate).grid(
+    column=3, row=3, sticky=W)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+mainframe.columnconfigure(2, weight=1)
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
